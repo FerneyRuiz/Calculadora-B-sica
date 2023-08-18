7920cc4c64eb38f4337d72994aa42c09948ece90
FerneyRuiz <137839864+FerneyRuiz@users.noreply.github.com>
2023-08-18 18:09:41 -0500

    Add files via upload
    Una pequeña calculadora basica en html, css y js

---
+++ scricp.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll(".number, .operator, .clear, .equal");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            if (this.classList.contains("clear")) {
+                display.value = "";
+            } else if (this.classList.contains("equal")) {
+                try {
+                    display.value = eval(display.value);
+                } catch (error) {
+                    display.value = "Error";
+                }
+            } else {
+                display.value += this.textContent;
+            }
+        });
+    });
+});
